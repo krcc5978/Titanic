f999167075aaa46e2112db4c1a5f1b8756e0f815
t-asai <t-asai@inbility.co.jp>
2021-12-10 16:44:10 +0900

    1.ランダムフォレストの追加

--- DecisionTree/decision_tree.py
+++ DecisionTree/decision_tree.py
@@ -1,8 +1,11 @@
+import numpy as np
 import pandas as pd
 from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
 
-train_data_path = "../titanic/train.csv"
-test_data_path = "../titanic/test.csv"
+
+train_data_path = '../titanic/train.csv'
+test_data_path = '../titanic/test.csv'
 
 
 def missing_table(df):
@@ -15,8 +18,10 @@ def missing_table(df):
 
 
 def prepare_data(table):
-    table["Age"] = table["Age"].fillna(table["Age"].median())
-    table["Embarked"] = table["Embarked"].fillna("S")
+    for i, data in enumerate(table['Age']):
+        if np.isnan(data):
+            table['Age'][i] = np.random.normal(table['Age'].mean(), table['Age'].std())
+    table['Embarked'] = table['Embarked'].fillna(np.random.randint(0,3))
 
     table['Sex'] = table['Sex'].replace('male', 0)
     table['Sex'] = table['Sex'].replace('female', 1)
@@ -27,18 +32,41 @@ def prepare_data(table):
 
 def decision_tree(train, test):
     # 「train」の目的変数と説明変数の値を取得
-    target = train["Survived"].values
-    features_one = train[["Pclass", "Sex", "Age", "Fare"]].values
+    target = train['Survived'].values
+    features_one = train[['Pclass', 'Sex', 'Age', 'Fare']].values
 
     # 決定木の作成
     my_tree_one = tree.DecisionTreeClassifier()
     my_tree_one = my_tree_one.fit(features_one, target)
 
     # 「test」の説明変数の値を取得
-    test_features = test[["Pclass", "Sex", "Age", "Fare"]].values
+    test_features = test[['Pclass', 'Sex', 'Age', 'Fare']].values
 
     # 「test」の説明変数を使って「my_tree_one」のモデルで予測
     return my_tree_one.predict(test_features)
+
+
+def random_forest(train, test):
+    target = train['Survived'].values
+    features_one = train[['Pclass', 'Sex', 'Age', 'Fare']].values
+
+    clf = RandomForestClassifier(random_state=4096)
+    clf.fit(features_one, target)
+
+    # 「test」の説明変数の値を取得
+    test_features = test[['Pclass', 'Sex', 'Age', 'Fare']].values
+
+    # 「test」の説明変数を使って「my_tree_one」のモデルで予測
+    return clf.predict(test_features)
+
+
+def make_kaggle_answer(my_prediction, test):
+    # PassengerIdを取得
+    PassengerId = np.array(test['PassengerId']).astype(int)
+    # my_prediction(予測データ）とPassengerIdをデータフレームへ落とし込む
+    my_solution = pd.DataFrame(my_prediction, PassengerId, columns=['Survived'])
+    # my_tree_one.csvとして書き出し
+    my_solution.to_csv('my_tree_one.csv', index_label=['PassengerId'])
 
 
 def check_survivor():
@@ -47,11 +75,12 @@ def check_survivor():
 
     prepare_data(train)
     prepare_data(test)
-    test["Fare"] = test["Fare"].fillna(test["Fare"].median())
+    test['Fare'] = test['Fare'].fillna(test['Fare'].median())
 
     print(missing_table(train))
     print(missing_table(test))
 
-    result = decision_tree(train, test)
-
+    result = random_forest(train, test)
     print(result)
+
+    make_kaggle_answer(result, test)
